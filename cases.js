bc5bd82e9c5033eaac78a911688f1021ea2847ba
Elowinagg <sigma9skibidi@gmail.com>
2026-03-20 22:01:11 +0100

    Create cases.js

---
+++ cases.js
@@ -0,0 +1,11 @@
+const cases = {
+  basic: {
+    price: 100,
+    items: [
+      {name: "AK-47 Safari Mesh", rarity: "common", img: "images/ak.png", chance: 50},
+      {name: "M4A1-S Guardian", rarity: "rare", img: "images/m4.png", chance: 30},
+      {name: "AWP Neo-Noir", rarity: "epic", img: "images/awp.png", chance: 15},
+      {name: "Karambit Doppler", rarity: "legendary", img: "images/knife.png", chance: 5}
+    ]
+  }
+};
