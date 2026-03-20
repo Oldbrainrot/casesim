5074df7a65c4f5693f1d311aa87d39d31a2f6a2b
Elowinagg <sigma9skibidi@gmail.com>
2026-03-20 21:54:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="UTF-8">
+  <title>Case Opening</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
+<h1>🎁 Otwórz skrzynkę</h1>
+
+<div class="case">
+  <div id="items"></div>
+</div>
+
+<button onclick="openCase()">OTWÓRZ</button>
+
+<h2 id="result"></h2>
+
+<script src="script.js"></script>
+</body>
+</html>
